f1a68cd1bf69a14a05dc8c237efaec876a229f01
vamshi-muktha <mukthavamshi123@gmail.com>
2025-12-08 09:49:08 +0000

    Assessment push to git

---
+++ Assessment/src/com/tcs/assignment/week1/OperatorEx.java
@@ -0,0 +1,14 @@
+package com.tcs.assignment.week1;
+
+public class OperatorEx {
+	public static void main(String[] args) {
+		Operator obj = new Operator(10, 20);
+		obj.sum();
+		obj.mul();
+		System.out.println("c : " + obj.c + "    d : " + obj.d);
+		obj.setValues(100, 200);
+		obj.sum();
+		obj.mul();
+		System.out.println("c : " + obj.c + "    d : " + obj.d);
+	}
+}
