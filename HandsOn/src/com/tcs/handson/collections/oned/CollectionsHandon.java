6dbf0b2a470fcd933d1756dd16ba8042b7fea66f
vamshi-muktha <mukthavamshi123@gmail.com>
2025-12-11 08:41:45 +0000

    Afternoon commit 2

--- HandsOn/src/com/tcs/handson/collections/oned/CollectionsHandon.java
+++ HandsOn/src/com/tcs/handson/collections/oned/CollectionsHandon.java
@@ -97,7 +97,7 @@ public class CollectionsHandon {
 
 	private static boolean isPrime(int i) {
 		if(i == 0 || i == 1) return false;
-		for(int j = 2; j < Math.sqrt(i); j++) if(i % 2 == 0) return false;
+		for(int j = 2; j <= Math.sqrt(i); j++) if(i % j == 0) return false;
 		return true;
 	}
 }
